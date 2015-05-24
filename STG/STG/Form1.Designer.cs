ca39bfa58b9f857853a725cda6a5c5e09d1fd110
Zake <zake7749@gmail.com>
2015-05-24 14:27:53 +0800

    fundmental framework

---
+++ STG/STG/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace STG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Update = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Update
+            // 
+            this.Update.Enabled = true;
+            this.Update.Interval = 15;
+            this.Update.Tick += new System.EventHandler(this.FixedUpdate);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(622, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(624, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-7, -29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(736, 633);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 601);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Update;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ STG/STG/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STG
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> enemy;
+        List<PictureBox> enemyBullet;
+        int est;
+        List<Bullet> playerBullet;
+        Player player;
+        Button b;
+        public Form1()
+        {
+            playerBullet = new List<Bullet>();
+            InitializeComponent();
+            player = new Player(100, 100);
+            this.panel1.Controls.Add(player.img);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void movePlayer()
+        {
+            if(player.canMove())
+            {
+                player.Move();
+            }
+        }
+
+        private void movePlayerBullet()
+        {
+            foreach(Bullet b in playerBullet)
+            {
+                b.Move();
+            }
+        }
+
+        private void FixedUpdate(object sender, EventArgs e)
+        {
+            movePlayer();
+            movePlayerBullet();
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void player_CreateBullet()
+        {
+            if(player.canShoot())
+            {
+                Point xy = player.getShootPlace();
+                Bullet b = new Bullet(xy.X,xy.Y);
+                this.panel1.Controls.Add(b.img);
+                playerBullet.Add(b);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    player.addV(0, -1);
+                    break;
+                case Keys.Down:
+                    player.addV(0, 1);
+                    break;
+                case Keys.Right:
+                    player.addV(1, 0);
+                    break;
+                case Keys.Left:
+                    player.addV(-1, 0);
+                    break;
+                case Keys.Space:
+                    player_CreateBullet();
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    player.vy =0;
+                    break;
+                case Keys.Down:
+                    player.vy =0;
+                    break;
+                case Keys.Right:
+                    player.vx =0;
+                    break;
+                case Keys.Left:
+                    player.vx = 0;
+                    break;
+            }
+        }
+    }
+
+    public class GameObject
+    {
+        public int lx,ly;//location
+        public int vx,vy;//velocity
+        public int vxupLimit,vyupLimit;//
+        public int vxdownLimit,vydownLimit;//
+        public int clock, clockLimit;//for update
+        public int move, moveLimit;//for update
+        public PictureBox img;
+
+        public GameObject(int x,int y)
+        {
+
+        }
+
+        public Boolean canShoot()
+        {
+            clock++;
+            if (clock > clockLimit)
+            {
+                clock = 0;
+                return true;
+            }
+            return false;
+        }
+
+        public Point getShootPlace()
+        {
+            Point p = new Point(lx+20, ly);
+            return p;
+        }
+
+        public Boolean canMove()
+        {
+            move++;
+            if (move > moveLimit)
+            {
+                move = 0;
+                return true;
+            }
+            return false;
+        }
+
+        public void Move()
+        {
+            lx += vx;
+            ly += vy;
+            img.Location = new Point(lx, ly);
+        }
+    }
+
+    public class Player : GameObject
+    {
+        public Player(int x, int y) : base(x,y)
+        {
+            lx = x;
+            ly = y;
+            //f = frame = timer interval of FixUpdate 
+            clock = 0;
+            clockLimit = 1;//每隔 1f 有一發子彈
+            move = 0;
+            moveLimit = 0;//每隔 1f 可以移動 p+vx,p+vy.
+            vx = 0;
+            vxupLimit = 3;//x軸速度在 3~-3
+            vxdownLimit = -3;
+            vy = 0;
+            vyupLimit = 3;//y軸速度在 3~-3
+            vydownLimit = -3;
+            img = new System.Windows.Forms.PictureBox();
+            img.Location = new Point(lx,ly);
+            img.Image = Image.FromFile(Application.StartupPath + "\\assest\\player.png");
+        }
+
+        public void addV(int ax,int ay)
+        {
+            if(ax!=0)
+            {
+                if (ax > 0 && vx < vxupLimit)
+                    vx += ax;
+                if (ax < 0 && vx > vxdownLimit)
+                    vx += ax;
+            }
+            else
+            {
+                if (ay > 0 && vy < vyupLimit)
+                    vy += ay;
+                if (ay < 0 && vy > vydownLimit)
+                    vy += ay;
+            }
+        }
+     
+    }
+
+    public class Bullet : GameObject
+    {
+        public Bullet(int x, int y) : base(x, y)
+        {
+            lx = x;
+            ly = y;
+            //f = frame = timer interval of FixUpdate 
+            move = 0;
+            moveLimit = 0;//每隔 1f 可以移動 p+vx,p+vy.
+            vx = 0;
+            vy = -3;
+            img = new System.Windows.Forms.PictureBox();
+            img.Location = new Point(lx, ly);
+            img.Image = Image.FromFile(Application.StartupPath + "\\assest\\Bullet_black.png");
+        }
+    }
+}

ef76bf51b0bc358300fbf8f818bc663220b4dd70
Zake <zake7749@gmail.com>
2015-06-14 14:37:29 +0800

    add Bomber move method : Collision

--- STG/STG/Bomber.cs
+++ STG/STG/Bomber.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Diagnostics;
+using Microsoft.DirectX;
+using Microsoft.DirectX.DirectSound;
 
 namespace STG
 {
     class Bomber : Enemy
     {
         bool disposed = false;
-
+        private Boolean ShiftMode;
         public Bomber(int x,int y) : base(x,y)
         {
+            ShiftMode = false;
             lx = x;
             ly = y;
             vx = 0;
@@ -21,6 +30,33 @@ namespace STG
             imgAutoSize();
             Shootmode = "None";
             health = 5;
+        }
+
+        public void OpenShiftMode()
+        {
+            ShiftMode = true;
+            vy = 1;
+            vx = -3.5;
+        }
+
+        public override void Move()
+        {
+            if (ShiftMode)
+            {
+                FixX();
+            }
+            lx += vx;
+            ly += vy;
+            img.Location = new Point(Convert.ToInt32(lx), Convert.ToInt32(ly));
+            img.BackColor = Color.Transparent;
+        }
+
+        private void FixX()
+        {
+            if (lx > 550)
+                vx = -4;
+            else if (lx < 20)
+                vx = 4;
         }
 
        //Dispose method

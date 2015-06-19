10e76063ac00df57b4d7f216062b7fce89728f4e
hank031014 <hank031014@gmail.com>
2015-06-19 23:43:20 +0800

    FunctionObject.cs
    特殊物件類別：可以回血、得到大量分數、讓player在一段時間內完全不受傷害
    PS. 尚未完成

---
+++ STG/STG/FunctionObject.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Drawing;
+
+namespace STG
+{
+    class FunctionObject : GameObject
+    {
+        Boolean disposed = false;
+        private int ObjType;//0:+10~20HP, 1:+30~50HP, 2:+HP full, 3:player become invincible, 4:+100~200 Score, 5:+300~500 Score, 6:+1000 Score
+        protected string[] imageFile = { };
+
+        public FunctionObject(int x, int y)
+             : base(x, y)
+        {
+            lx = x;
+            ly = y;
+            vx = 0;
+            vy = 1;
+            move = 0;
+            moveLimit = 0;
+            DecideType();
+            LoadImage(ObjType);
+
+        }
+
+        private void DecideType()
+        {
+            Random decide = new Random(Guid.NewGuid().GetHashCode());
+            ObjType = decide.Next(0, 6);
+
+        }
+
+        private void LoadImage(int type)
+        {
+            img = new System.Windows.Forms.PictureBox();
+            img.Location = img.Location = new Point(Convert.ToInt32(lx), Convert.ToInt32(ly));
+            img.Image = Image.FromFile(Application.StartupPath + "\\assest\\Enemy\\" + imageFile[type]);
+            img.BackColor = Color.Transparent;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here.
+                //
+            }
+
+            // Free any unmanaged objects here.
+            //
+            disposed = true;
+        }
+
+        ~FunctionObject()
+        {
+            Dispose(false);
+        }
+    }
+}
